da216cadce8f8d4c897284d1b727a6fadafc4e37
bridger <48978583+bridgerhung@users.noreply.github.com>
2023-08-26 05:06:30 +0000

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,14 +4,14 @@ import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: "廢棄地區(7/30過期)",
+    //title: "廢棄地區(7/30過期)",
     //  description: <>DNS1:&nbsp;&nbsp;140.125.252.1&nbsp;&nbsp;</>,
     //  description2: <>DNS2:&nbsp;&nbsp;140.125.253.2&nbsp;&nbsp;</>,
-    link1: (
-      <>
-        <a href={"https://blog-bil3k39tb-bridgerhung.vercel.app/"}>&nbsp;封存課程和學習歷程&nbsp;</a>
-      </>
-    ),
+    // link1: (
+    //   <>
+    //     <a href={"https://blog-bil3k39tb-bridgerhung.vercel.app/"}>&nbsp;封存課程和學習歷程&nbsp;</a>
+    //   </>
+    // ),
     // link2: <><a href={'/class/en/TV%20introduce'}>&nbsp;六人行&nbsp;</a></>,
     // link3: <><a href={'/class/MIS/mobile_system'}>&nbsp;行動系統&nbsp;</a></>
   },
