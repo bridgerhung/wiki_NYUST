c5d695529e4a088dcad8553e75680b2cc35bb35c
Bridger Hung <jnkjko123@gmail.com>
2024-12-27 08:08:11 +0800

    移除 @vercel/speed-insights 依賴以簡化配置文件

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,6 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import { themes as prismThemes } from "prism-react-renderer";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
