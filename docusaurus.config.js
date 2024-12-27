0992545ccde921e0741651448163edc469295b18
Bridger Hung <jnkjko123@gmail.com>
2024-12-27 08:06:37 +0800

    新增 @vercel/speed-insights 依賴以提升性能分析功能

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,6 +5,7 @@
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
 import { themes as prismThemes } from "prism-react-renderer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
